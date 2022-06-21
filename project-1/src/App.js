6a7d23c9a60abafbcc3ce733193dae46f99261a0
leofontolan <leofontolan@gmail.com>
2022-06-21 16:53:09 -0300

    Array Map

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,28 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "O título 1",
+        body: "O corpo 1",
+      },
+      {
+        id: 2,
+        title: "O título 2",
+        body: "O corpo 2",
+      },
+      {
+        id: 3,
+        title: "O título 3",
+        body: "O corpo 3",
+      },
+    ],
+  };
 
-        <h1>Olá Mundo!</h1>
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    //Atribuição por Destructuring
+    const { posts } = this.state;
+
+    return (
+      <div className="App">
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
